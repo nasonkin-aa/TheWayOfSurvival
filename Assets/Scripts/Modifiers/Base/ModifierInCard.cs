5045f24844d9644a052f96380d0865e2ad018837
Native <Native095@yandex.ru>
2023-10-31 18:14:22 +1000

    Add modifiers pool

--- Assets/Scripts/Modifiers/Base/ModifierInCard.cs
+++ Assets/Scripts/Modifiers/Base/ModifierInCard.cs
@@ -13,12 +13,11 @@ public enum ModifierName
 public class ModifierInCard : MonoBehaviour
 {
     public ModifierTarget modifierTarget;
-    public Player player;
     public ModifierName modifierName;
 
     public void Activate ()
     {
         var mod = new ModifierPrepare(modifierName.ToString());
-        ModifierAdder.AddModifier(mod, player, modifierTarget);
+        ModifierAdder.AddModifier(mod, PlayerStatic.PlayerScript, modifierTarget);
     }
 }
---
+++ Assets/Scripts/Modifiers/Draw/ModifiersPool.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifiersPool : MonoBehaviour
+{
+    [SerializeField]
+    public List<ModifierInCard> modifiers;
+
+    private void Awake()
+    {
+
+    }
+}
