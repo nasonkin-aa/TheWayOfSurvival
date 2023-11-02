e3d037c5a5e2f0dc02f2c8d5d7060a658e60e40b
Native <Native095@yandex.ru>
2023-11-02 17:52:07 +1000

    Fix jumping

--- Assets/Scripts/Character/GroundChecker.cs
+++ Assets/Scripts/Character/GroundChecker.cs
@@ -6,13 +6,14 @@ public class GroundChecker : MonoBehaviour
     public static bool IsPayerOnTheGround { get; private set; }
     private Collider2D _groundCollider;
     private ContactFilter2D _contactFilter;
-    private Animator _animatorPlayer;
+    private Animator AnimatorPlayer => gameObject.GetComponentInParent<Animator>();
+    private readonly LayerMask _jumpingMask = (1 << 10) | (1 << 8); // Mask include Ground(10) and Building(8)
 
     private void Awake()
     {
         _contactFilter = new ContactFilter2D().NoFilter();
         _contactFilter.useLayerMask = true;
-        _contactFilter.layerMask = ~LayerMask.GetMask("Player");
+        _contactFilter.layerMask = _jumpingMask;
         _groundCollider = GetComponent<Collider2D>();
     }
 
@@ -22,12 +23,12 @@ public class GroundChecker : MonoBehaviour
         if (_groundCollider.OverlapCollider(_contactFilter, list) > 0)
         {
             IsPayerOnTheGround = true;
-            gameObject.GetComponentInParent<Animator>().SetBool("IsJumping", false);
+            AnimatorPlayer.SetBool("IsJumping", false);
         }
         else
         {
             IsPayerOnTheGround = false;
-            gameObject.GetComponentInParent<Animator>().SetBool("IsJumping", true);
+            AnimatorPlayer.SetBool("IsJumping", true);
         }
             
     }
