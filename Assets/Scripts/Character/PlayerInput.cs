2205d8235999946e20b8a049b0f1544fb6de3288
Native <Native095@yandex.ru>
2023-11-02 15:32:15 +1000

    Add simple pause for check
    Press button "P" for pause

--- Assets/Scripts/Character/PlayerInput.cs
+++ Assets/Scripts/Character/PlayerInput.cs
@@ -5,38 +5,57 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMove))]
 public class PlayerInput : MonoBehaviour
 {
-
-   public static float Horizontal;
-   public static float Vertical;
-   public static PlayerInput current;
+    public static float Horizontal;
+    public static float Vertical;
+    public static PlayerInput current;
    
-   public static Action<float> OnPlayerMoveHorizontal;
-   public static Action OnPlayerJump;
-   public static Action<Vector3> OnPlayerAttack;
-   public static Action<Vector3> OnPlayerFlip;
+    public static Action<float> OnPlayerMoveHorizontal;
+    public static Action OnPlayerJump;
+    public static Action<Vector3> OnPlayerAttack;
+    public static Action<Vector3> OnPlayerFlip;
+    public bool IsInputBlock = false;
 
-   private void Awake() //Maybe this not need??
-   {
-      current = this;
-   }
 
-   public void FixedUpdate()
-   {
-      Horizontal = Input.GetAxis("Horizontal");
-      OnPlayerMoveHorizontal(Horizontal);
-      gameObject.GetComponent<Animator>().SetFloat("Speed", MathF.Abs(Horizontal));
-   }
+    private void Awake() //Maybe this not need??
+    {
+        current = this;
+    }
 
-   public void Update()
-   {
-      if (Input.GetButtonDown("Fire1"))
-      {
-         OnPlayerAttack(Input.mousePosition);
-      }
+    public void FixedUpdate()
+    {
+        if (IsInputBlock)
+            return;
+
+        Horizontal = Input.GetAxis("Horizontal");
+        OnPlayerMoveHorizontal(Horizontal);
+        gameObject.GetComponent<Animator>().SetFloat("Speed", MathF.Abs(Horizontal));
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            PauseSwitch();
+
+        if (IsInputBlock)
+            return;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            OnPlayerAttack(Input.mousePosition);
+        }
       
-      if (Input.GetButtonDown("Jump"))
-         OnPlayerJump();
+        if (Input.GetButtonDown("Jump"))
+            OnPlayerJump();
 
-      OnPlayerFlip(Input.mousePosition);
-   }
+        OnPlayerFlip(Input.mousePosition);
+    }
+
+    public void PauseSwitch()
+    {
+        IsInputBlock = !IsInputBlock;
+        if (Time.timeScale == 0)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+    }
 }
