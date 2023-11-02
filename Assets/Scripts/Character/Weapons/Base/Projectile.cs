607bf3e185f054d9a76172a2817afc6a67cedb0c
Brentimus <Gamepopins@gmail.com>
2023-11-02 19:29:16 +1000

    Hack with projectile

--- Assets/Scripts/Character/Weapons/Base/Projectile.cs
+++ Assets/Scripts/Character/Weapons/Base/Projectile.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    protected internal int Damage = 10;
+    protected internal int Damage;
+    private bool _isContact = false;
     public Action OnProjectileCollision;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_isContact) return; //Hack for kill 1 enemy 1 projectile
+        
         collision.gameObject.GetComponent<Health>()?.TakeDamage(Damage);
 
         if (OnProjectileCollision is not null) //Modifier check
             OnProjectileCollision();
-
+        _isContact = true;
         Destroy(gameObject);
     }
+
 }
