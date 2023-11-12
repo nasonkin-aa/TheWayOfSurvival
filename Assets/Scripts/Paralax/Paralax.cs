67cd68cfa69c037fcbfcd991c10cd1a66d5296cc
Brentimus <Gamepopins@gmail.com>
2023-11-12 22:34:40 +1000

    Make paralax effect for part of background, change sprite ground

--- Assets/Scripts/Paralax/Paralax.cs
+++ Assets/Scripts/Paralax/Paralax.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Paralax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform followingTarget;
+    [SerializeField, Range(0f, 1f)] private float parallaxStrength;
+    [SerializeField] private bool disableVerticalParallax;
+    [SerializeField] private bool invertParallax;
+    private Vector3 _targetPreviousPosition;
+
     void Start()
     {
-        
+        if (Camera.main != null) 
+            followingTarget = Camera.main.transform;
+        else
+            Debug.LogError("Camera not detected");
+        _targetPreviousPosition = followingTarget.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var delta = followingTarget.position - _targetPreviousPosition;
+
+        if (disableVerticalParallax)
+            delta.y = 0;
+        _targetPreviousPosition = followingTarget.position;
+        if (!invertParallax)
+            transform.position += delta * parallaxStrength;
+        else
+            transform.position -= delta * parallaxStrength;
     }
 }
