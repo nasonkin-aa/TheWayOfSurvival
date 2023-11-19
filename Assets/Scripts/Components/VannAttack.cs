e2bc1a862542ca975dad6a6bcd1bd5be972d4de3
Native <Native095@yandex.ru>
2023-11-19 18:28:58 +1000

    Add Vann attack

---
+++ Assets/Scripts/Components/VannAttack.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+public class VannAttack : Attack
+{
+    [SerializeField] protected GameObject _projectilePrefab;
+    [SerializeField] protected float launchAngle = 45f;
+    protected bool _isReadyToAttack = true;
+    protected float _reloadTime = 5f;
+    [SerializeField] protected Transform _SpawnPoint;
+    [SerializeField] protected float _firePower = 20f;
+
+    public override void ContactWithTarget(HealthBase health)
+    {
+        if (!_isReadyToAttack)
+            return;
+
+        _targetToAttack = health;
+        OnAttackReady?.Invoke();
+        _isReadyToAttack = false;
+        StartCoroutine(Reload());
+    }
+
+    void FireProjectile(Transform target)
+    {
+        Vector2 direction = _targetToAttack.transform.position - transform.position;
+        var directionSign = Mathf.Sign(direction.x);
+
+        GameObject projectile = Instantiate(_projectilePrefab, _SpawnPoint.position, Quaternion.identity);
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        var yDistance = -(target.position.y - transform.position.y);
+        if (Mathf.Abs(yDistance) < 2)
+            yDistance = 0;
+
+        var distance = Mathf.Abs((target.position.x - transform.position.x) / 2 + yDistance); // until target
+        var vx = Mathf.Sqrt(distance * (-Physics2D.gravity.y) / 2); // x velicity for flying distance
+        var power = vx / Mathf.Cos(45);
+
+        rb.velocity = new Vector2(directionSign, 1).normalized * (power); // 45 degry (vector) * magnitude
+    }
+
+    public void Attack ()
+    {
+        FireProjectile(_targetToAttack.transform);
+    }
+
+    private IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(_reloadTime);
+        _isReadyToAttack = true;
+    }
+}
---
+++ Assets/Scripts/Enemy/Projectiles/VannArrow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class VannArrow : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var direction = _rb.velocity.normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
