c8500c7c4e1f42c70825d2e0db82162de377a0b0
AlexM4096 <alex.m.game.jam@gmail.com>
2024-10-11 22:53:50 +1000

    Add tutorial button

--- Assets/Scripts/SceneSelector.cs
+++ Assets/Scripts/SceneSelector.cs
@@ -4,4 +4,5 @@ using UnityEngine.SceneManagement;
 public class SceneSelector : MonoBehaviour
 {
     public void SelectGame() => SceneManager.LoadScene("Game");
+    public void SelectTutorial() => SceneManager.LoadScene("Tutorial");
 }
